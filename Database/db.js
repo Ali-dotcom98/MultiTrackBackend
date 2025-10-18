f594a399c7c38e571a863418891897ea01b258d3
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-10-18 21:45:16 +0500

    Update

--- Database/db.js
+++ Database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async (Url) => {
     try {
-        await mongoose.connect(Url)
+        await mongoose.connect("mongodb://127.0.0.1:27017/TaskManagment")
         console.log("MongoDb is Connected");
 
     } catch (error) {
